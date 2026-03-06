84d143f95ddd27ab012b8ad90dab1ae3e008829e
Sarai Villalta <sarai.villalta12@gmail.com>
2026-03-06 00:50:41 -0600

    Login screen and basic authentication logic

--- app/src/main/java/com/example/goldmoon_bistro/MainActivity.java
+++ app/src/main/java/com/example/goldmoon_bistro/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.goldmoon_bistro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtusuario, edtclave;
+    Button btningresar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edtusuario = findViewById(R.id.edtUsuario);
+        edtclave = findViewById(R.id.edtClave);
+
+        btningresar = findViewById(R.id.btnIngresar);
+
+        btningresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = edtusuario.getText().toString().trim();
+                String clave = edtclave.getText().toString().trim();
+
+                if(usuario.isEmpty() || clave.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Complete los campos", Toast.LENGTH_SHORT).show();
+                } else if (!usuario.equals("LilianaVillalta") || !clave.equals("goldmoon123")) {
+                    Toast.makeText(getApplicationContext(), "Usuario o contraseña incorrectos" , Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Bienvenid@ " +usuario , Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Registro.class);
+
+                    intent.putExtra("usuario", usuario);
+
+                    startActivity(intent);
+                }
+            }
         });
+
+
     }
 }
